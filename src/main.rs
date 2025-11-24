22b379d024b8525c853e0ef4316bc576b2316110
Maxime Laudrin <maxime.laudrin@protonmail.com>
2025-11-24 11:59:50 +0100

    Draft grow init command

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,18 @@
+use std::env;
+use std::path::PathBuf;
 use clap::{Parser, Subcommand};
+
+// Actions:
+// - Create class schema
+// - Validate class schema
+// - Create class instance
+// - Validate instances against class schema
+
+// grow class
+//   - create
+//   - remove (?)
+//   - check
+// grow inst[ance]
 
 #[derive(Debug, Parser)]
 #[command(name = "grow")]
@@ -12,9 +26,20 @@ struct Cli {
 enum Commands {
     #[command(arg_required_else_help = true)]
     Init {
-        name: String,
-    },
+        class_name: String,
 
+        #[arg(default_value = ".")]
+        directory: PathBuf,
+
+        #[arg(short = 'e', long = "editor")]
+        editor_opt: Option<String>,
+
+        #[arg(short = 'E', long = "no-edit")]
+        no_edit: bool,
+
+        #[arg(short = 'v', long = "verbose")]
+        verbose: bool,
+    },
     Run {
     },
 }
@@ -34,11 +59,56 @@ fn main() {
     let cli = Cli::parse();
 
     match cli.command {
-        Commands::Init { name } => {
-            println!("Init {name}")
+        Commands::Init { class_name, directory, editor_opt, no_edit, verbose } => {
+            eprintln!("TODO: ensure data dir.");
+            eprintln!("TODO: ensure datasets file.");
+
+            eprintln!("TODO: create dir [{}/.grow].", directory.display());
+            eprintln!("TODO: create entry for {class_name}.");
+
+            eprintln!("TODO: create dir [./{class_name}].");
+            eprintln!("TODO: create dir [./{class_name}/instances].");
+            eprintln!("TODO: create file [./{class_name}/schema.yml].");
+
+            if !no_edit {
+                let editor = resolve_editor(&editor_opt, verbose);
+                eprintln!("TODO: open file [./{class_name}/schema.yml] with {editor}.");
+            }
         }
         Commands::Run { } => {
-            println!("Run")
+            eprintln!("Run")
         }
     }
 }
+
+
+fn resolve_editor(editor_opt: &Option<String>, verbose: bool) -> String {
+    if let Some(editor) = editor_opt.as_deref() {
+        if verbose {
+            eprintln!("Using editor from CLI argument: {}", editor);
+        }
+        return editor.to_string()
+    }
+
+    if let Ok(editor) = env::var("VISUAL") {
+        if verbose {
+            eprintln!("Using editor from VISUAL environment variable: {}", editor);
+        }
+        return editor
+    }
+
+    if let Ok(editor) = env::var("EDITOR") {
+        if verbose {
+            eprintln!("Using editor from EDITOR environment variable: {}", editor);
+        }
+        return editor
+    }
+
+    if verbose {
+        eprintln!("No editor specified, falling back to vim");
+    }
+    "vim".to_string()
+}
+
+
+
